47e640ee5e11d75cadd97db2d3dc0ad21ca2327b
hernanparada <ernan_parada@hotmail.com>
2015-12-04 01:51:23 -0500

    Agregando funciones Branches y Set

---
+++ Parser/funal.c
@@ -0,0 +1,91 @@
+#include "funal.h"
+
+
+//Funciones Branches
+int be(Node **node){
+	int ir = 0;
+	*node = (**node).next;
+	char *str = (**node).val.sval;
+
+	ir |= SET_BRA_FMT << OP;
+	ir |= BE << COND;
+	ir |= BRANCH << OP2;
+	ir |= atoi(str);
+	return 0;
+}
+
+int bcs(Node **node){
+	int ir = 0;
+	*node = (**node).next;
+	char *str = (**node).val.sval;
+
+	ir |= SET_BRA_FMT << OP;
+	ir |= BCS << COND;
+	ir |= BRANCH << OP2;
+	ir |= atoi(str);
+	return 0;
+}
+
+int bneg(Node **node){
+	int ir = 0;
+	*node = (**node).next;
+	char *str = (**node).val.sval;
+
+	ir |= SET_BRA_FMT << OP;
+	ir |= BNEG << COND;
+	ir |= BRANCH << OP2;
+	ir |= atoi(str);
+	return 0;
+}
+
+int bvs(Node **node){
+	int ir = 0;
+	*node = (**node).next;
+	char *str = (**node).val.sval;
+
+	ir |= SET_BRA_FMT << OP;
+	ir |= BVS << COND;
+	ir |= BRANCH << OP2;
+	ir |= atoi(str);
+	return 0;
+}
+
+int ba(Node **node){
+	int ir = 0;
+	*node = (**node).next;
+	char *str = (**node).val.sval;
+
+	ir |= SET_BRA_FMT << OP;
+	ir |= BA << COND;
+	ir |= BRANCH << OP2;
+	ir |= atoi(str);
+	return 0;
+}
+//Funcion set
+int sethi(Node **node){
+	int ir = 0;
+	ir |= SET_BRA_FMT << OP;
+	ir |= SETHI << OP2;
+	*node = (**node).next;
+	char *str = (**node).val.sval;
+	while(strcmp(str,"\n")!=0){
+		if(*str == '%'){
+			ir |= rton(str) << RD;
+		}else if((48<=*str) && (*str<=57)){
+			ir |= atoi(str);
+		}else{
+			entry *ent;
+			int val = getEntryKey(&symbols,str,&ent);
+			if(val<0)
+				printf("Error obteniendo ir para la funcion SETHI");
+				return -1;
+			else{
+				ir |= (*ent).value;
+			}
+		}
+		*node = (**node).next;
+		char *str = (**node).val.sval;		
+	}
+	return 0;
+}
+
---
+++ Parser/funal.h
@@ -0,0 +1,17 @@
+#ifndef funal.h
+#define funal.h
+
+#include "Node.h"
+#include "dictionary.h"
+
+//Prototipos branches
+int be(Node **node);
+int bcs(Node **node);
+int bneg(Node **node);
+int bvs(Node **node);
+int ba(Node **node);
+
+//Prototipo set
+int sethi(Node **node);
+
+#endif
