9396530fd58618b60449becd51ee96721ec600cc
elianacorradine <eli.corradine@hotmail.com>
2015-12-04 01:03:55 -0500

    Arreglo del  ejemplo add2Numbers
    Se hizo igual que el archivo main memory de este ejemplo

--- Assembler/Add 2 Numbers/add.asm
+++ Assembler/Add 2 Numbers/add.asm
@@ -2,9 +2,9 @@
 	.begin
 	.org 2048
 
-	x:15
+	x:112
 	y:3
-	z:0	
+	z:4096	
 prog: ld [x] , %r1 !Cargar x en el registro r1
       ld [y] , %r2 !Cargar y en el registro r2
       addcc %r1,%r2 ,%r3 !%r3 ← %r1 + %r2
