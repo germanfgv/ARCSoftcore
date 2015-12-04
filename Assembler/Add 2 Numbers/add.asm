b4e079e874117b59184dc3cb1bb3170c2cd79fc0
elianacorradine <eli.corradine@hotmail.com>
2015-12-04 05:16:58 -0500

    Ubicación de variables al final

--- Assembler/Add 2 Numbers/add.asm
+++ Assembler/Add 2 Numbers/add.asm
@@ -1,16 +1,13 @@
 !Este programa realiza la suma de dos números previamente cargado
 	.begin
 	.org 2048
-
-	x:112
-	y:3
-	z:4096	
 prog: ld [x] , %r1 !Cargar x en el registro r1
       ld [y] , %r2 !Cargar y en el registro r2
       addcc %r1,%r2 ,%r3 !%r3 ← %r1 + %r2
       st %r3, [z] !Guardar el Resultado en z
-
-
+      	x:112
+	y:3
+	z:4096
 	.end
 
 
